faa49af98f21b2c16d2d2172c5d5f229dd29ded5
Juliana Nascimento <seu-email-do-github@email.com>
2026-01-14 16:51:59 -0300

    feat: grant cluster admin access to IAM user for kubectl access

--- eks.tf
+++ eks.tf
@@ -13,6 +13,21 @@ module "eks" {
 
   enable_irsa = true
 
+  access_entries = {
+    admin = {
+      principal_arn = "arn:aws:iam::626635426531:user/terraform-user"
+
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
+
   eks_managed_node_groups = {
     default = {
       name = "managed-ng"
